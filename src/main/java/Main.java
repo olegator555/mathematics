6f2f7c7d5b834b1a876c6c05b0fc4b0892b7b7dc
olegator <o.shashko@icloud.com>
2021-10-03 11:59:00 +0300

    IndexOutOfBoundsException fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.util.stream.IntStream;
 public class Main {
 
 
-    public static void main(String[] args) throws OutOfRangeException {
+    public static void main(String[] args) {
         final int[] x_nodes = IntStream.range(1995,2019).toArray();
         final double[] y_nodes = {14285,20078,23425,26296,48232,73056,89436,108305,132082,170272,216098,269172,332475,412768,388072,463085,602825,681639,731339,791997,832326,860102,920893,1036266};
 
@@ -14,14 +14,21 @@ public class Main {
         functions.print_all_coefficients();
 
         Scanner scanner = new Scanner(System.in);
-        int x_point = 0;
+        int x_point;
         System.out.print("Enter x: ");
         try {
             x_point = scanner.nextInt();
         } catch (InputMismatchException e) {
-            System.out.println("Integer only");
+            throw new RuntimeException("Integer only");
         }
-        double[] qs_coeff = functions.get_coefficients_matrix(x_point,x_nodes);
+
+        double[] qs_coeff = new double[0];
+        try {
+            qs_coeff = functions.get_coefficients_matrix(x_point,x_nodes);
+        } catch (OutOfRangeException e) {
+            System.out.println("x must be between 1995 and 2017");
+        }
+
         double spline = functions.qubic_spline(x_point,qs_coeff);
         double spline_derivative = functions.d_qubic_spline(x_point,qs_coeff);
         System.out.println("P(x)="+spline);
